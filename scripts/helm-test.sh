467a1ec88de74189d11e4b408d880e792be0befa
alpha <agg.shikha@gmail.com>
2021-07-23 10:03:00 +0100

    pipeline 1.1

--- scripts/helm-test.sh
+++ scripts/helm-test.sh
@@ -14,12 +14,13 @@ helm template pages
 
 echo "------------------------Start time is--------  $(date +%Y-%m-%dT%H%M%S%z)"
 
-helm upgrade --install "$RELEASE_NAME" pages --debug --timeout 120s  --wait
+#helm upgrade --install "$RELEASE_NAME" pages --debug --timeout 120s  --wait
+helm install "$RELEASE_NAME" pages --debug
 
 echo "------------------------End time is--------  $(date +%Y-%m-%dT%H%M%S%z)"
 
 echo '---------------------Started testing--------------'
-sleep 10s
+sleep 60s
 kubectl get po -n "$NAMESPACE" --show-labels
 kubectl get svc -n "$NAMESPACE" -o wide
 helm test "$RELEASE_NAME" --logs
